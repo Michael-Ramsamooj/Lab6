64eebbc2fa004f9f44c06c19d731523120b490e7
Davidbick <david.sony2003@gmail.com>
2023-10-25 14:13:28 -0400

    added decoder

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 def encode(number):
     result = ""
-    for i in str(number):
+    for i in number:
         num = int(i) + 3
         result += str(num)
+    print(result)
     return result
+
+
+def decode(password):
+    final = ""
+    for i in str(password):
+        num = int(i) - 3
+        final += str(num)
+    return final
 
 
 def main():
@@ -12,11 +21,12 @@ def main():
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
         user_input = int(input("Please enter an option: "))
         if user_input == 1:
-            password = int(input("Please enter your password to encode:"))
-            encode(password)
+            password = input("Please enter your password to encode:")
+            encoded = encode(password)
             print("Your password has been encoded and stored!")
         if user_input == 2:
-            pass
+            decoded = decode(encoded)
+            print(f"The encoded password is {encoded}, and the original password is {decoded}.")
         if user_input == 3:
             x = 0
 
