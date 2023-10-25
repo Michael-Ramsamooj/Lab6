d19868c29ff517cb32a3f8688d100dddc555fefc
Michael Ram <ramsamoojm@ufl.edu>
2023-10-25 13:19:32 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def encoder(number):
+    for i in number:
+        num = number + 3
+        str(num)
+
+def main()
+    # print menu and getting user input
+
+if __name__ == '__main__':
+    main()
